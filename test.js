cf8f3aa35039ca0e78846ffc69189d3a6a5b788f
shatha <shathaamin63@gmail.com>
2023-03-29 13:28:49 +0300

    add test file closes #10

--- test.js
+++ test.js
@@ -1,30 +1,28 @@
-/*const chai = require('chai');
-const chaiHttp = require('chai-http');
-const app = require('./app');
+const request = require('supertest');
+const app = require('./src/router');
 
-chai.use(chaiHttp);
-chai.should();
+test('GET / should respond with status 200', done => {
+  request(app)
+    .get('/')
+    .expect(200, done);
+});
 
-describe('Countries API', () => {
-  describe('GET /countries/:value', () => {
-    it('should return an array of matching countries', (done) => {
-      chai.request(app)
-        .get('/countries/ger')
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.an('array');
-          res.body.should.include('Germany');
-          done();
-        });
-    });
-    it('should return an empty array if no matches are found', (done) => {
-      chai.request(app)
-        .get('/countries/xyz')
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.an('array').that.is.empty;
-          done();
-        });
-    });
-  });
-});*/
+test('GET /non-existent-route should respond with status 404', done => {
+  request(app)
+    .get('/non-existent-route')
+    .expect(404, done);
+});
+
+test('GET /styles.css should respond with status 200 and the CSS file', done => {
+  request(app)
+    .get('/styles.css')
+    .expect(200)
+    .expect('Content-Type', 'text/css; charset=UTF-8', done);
+});
+
+test('GET /app.js should respond with status 200 and the JavaScript file', done => {
+  request(app)
+    .get('/app.js')
+    .expect(200)
+    .expect('Content-Type', 'application/javascript; charset=UTF-8', done);
+});
